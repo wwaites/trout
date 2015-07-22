b91bea11dd8376635a464bb292475d42e8e04771
William Waites <wwaites@tardis.ed.ac.uk>
2015-07-22 07:30:53 +0100

    cosmetic change to make godoc work better

--- trout.go
+++ trout.go
@@ -1,5 +1,4 @@
 /*
-    trout - a program that reactively diagnoses network faults
     Copyright (C) 2015 William Waites
 
     This program is free software: you can redistribute it and/or modify
@@ -14,6 +13,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+    trout - a program that reactively diagnoses network faults
  */
 package main
 
