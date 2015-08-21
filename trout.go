8562e6f1f8c45de3b5dd2f2fcee363398e4812b8
William Waites <wwaites@tardis.ed.ac.uk>
2015-08-21 22:04:21 +0100

    go fmt

--- trout.go
+++ trout.go
@@ -1,23 +1,23 @@
 /*
-    Copyright (C) 2015 William Waites
+   Copyright (C) 2015 William Waites
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*
-    trout - a program that reactively diagnoses network faults
- */
+   trout - a program that reactively diagnoses network faults
+*/
 package main
 
 import (
@@ -31,6 +31,7 @@ import (
 var fping string
 var traceroute string
 var numeric bool
+
 func init() {
 	flag.BoolVar(&numeric, "n", false, "Do not resolve addresses")
 	flag.Usage = func() {
@@ -50,7 +51,7 @@ License version 3 or later.
 	if err != nil {
 		panic(err)
 	}
-	
+
 	traceroute, err = exec.LookPath("traceroute")
 	if err != nil {
 		panic(err)
